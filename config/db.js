7f944536d5bfb3492cd0cd24d46874e25746cddd
sibykarthikeyan <siby.karthii@gmail.com>
2020-09-10 12:20:53 +0530

    Added the url

--- config/db.js
+++ config/db.js
@@ -4,6 +4,6 @@ const env = process.env.NODE_ENV ? process.env.NODE_ENV : "development";
 const configEnv = config[env];
 
 module.exports ={
-    url : `mongodb://${configEnv.host}:27017/`+`${configEnv.database}`
-    //url : `mongodb+srv://wolf:English@123@cluster0-frpa3.mongodb.net/orders_db?retryWrites=true&w=majority`
+    //url : `mongodb://${configEnv.host}:27017/`+`${configEnv.database}`
+    url : `mongodb+srv://wolf:English@123@cluster0-frpa3.mongodb.net/orders_db?retryWrites=true&w=majority`
 }
